e65623b1da8282567aed92a21c82571862cef64e
Victor Tseng <palatis@Nichelle.palatis.iiiii.info>
2012-09-06 23:57:43 +0800

    real initial commit.
    adds the SoftPWM library files and an example.

---
+++ SoftPWM.h
@@ -0,0 +1,195 @@
+#ifndef _SOFTPWM_H_
+#define _SOFTPWM_H_
+
+#include <Arduino.h>
+
+/* helper macros */
+#define SOFTPWM_DEFINE_PINMODE( CHANNEL, PMODE, PORT, BIT ) \
+  template < > \
+  inline void pinModeStatic< CHANNEL >( uint8_t const mode ) \
+  { \
+    if (mode == INPUT) { \
+      bitClear(PMODE, BIT); bitClear(PORT, BIT); \
+    } else if (mode == INPUT_PULLUP) { \
+      bitClear(PMODE, BIT); bitSet(PORT, BIT); \
+    } else { \
+      bitSet(PMODE, BIT); \
+    } \
+  }
+
+#define SOFTPWM_DEFINE_CHANNEL( CHANNEL, PMODE, PORT, BIT ) \
+  template < > \
+  inline void bitWriteStatic< CHANNEL >( bool const value ) \
+  { bitWrite( PORT, BIT, value ); } \
+  SOFTPWM_DEFINE_PINMODE( CHANNEL, PMODE, PORT, BIT )
+  
+#define SOFTPWM_DEFINE_CHANNEL_INVERT( CHANNEL, PMODE, PORT, BIT ) \
+  template < > \
+  inline void bitWriteStatic< CHANNEL >( bool const value ) \
+  { bitWrite( PORT, BIT, !value ); } \
+  SOFTPWM_DEFINE_PINMODE( CHANNEL, PMODE, PORT, BIT )
+
+#define SOFTPWM_DEFINE_OBJECT_WITH_BRIGHTNESS_LEVELS( CHANNEL_CNT, BRIGHTNESS_LEVELS ) \
+  CSoftPWM< CHANNEL_CNT, BRIGHTNESS_LEVELS > SoftPWM; \
+  ISR(TIMER1_COMPA_vect) { sei(); SoftPWM.update(); }
+
+#define SOFTPWM_DEFINE_OBJECT( CHANNEL_CNT ) \
+  SOFTPWM_DEFINE_OBJECT_WITH_BRIGHTNESS_LEVELS( CHANNEL_CNT, 0 )
+
+#define SOFTPWM_DEFINE_EXTERN_OBJECT_WITH_BRIGHTNESS_LEVELS( CHANNEL_CNT, BRIGHTNESS_LEVELS ) \
+  extern CSoftPWM< CHANNEL_CNT, BRIGHTNESS_LEVELS > SoftPWM;
+  
+#define SOFTPWM_DEFINE_EXTERN_OBJECT( CHANNEL_CNT ) \
+  SOFTPWM_DEFINE_EXTERN_OBJECT_WITH_BRIGHTNESS_LEVELS( CHANNEL_CNT, 0 )
+
+/* here comes the magic @o@ */
+template < int channel > inline void bitWriteStatic( bool value ) {}
+template < int channel > inline void pinModeStatic( uint8_t mode ) {}
+
+template < int channel >
+struct bitWriteStaticExpander
+{
+  void operator() ( bool value ) const
+  {
+    bitWriteStatic< channel >( value );
+    bitWriteStaticExpander< channel - 1 >()( value );
+  }
+
+  void operator() ( uint8_t const &count, uint8_t const * const &channels ) const
+  {
+    bitWriteStatic< channel >( count < channels[ channel ] );
+    bitWriteStaticExpander< channel - 1 >()( count, channels );
+  }
+};
+
+template < >
+struct bitWriteStaticExpander< -1 >
+{
+  void operator() ( bool ) const {}
+  void operator() ( uint8_t const &, uint8_t const * const & ) const {}
+};
+
+template < int channel >
+struct pinModeStaticExpander
+{
+  void operator() ( uint8_t const mode ) const
+  {
+    pinModeStatic< channel >( mode );
+    pinModeStaticExpander< channel - 1 >()( mode );
+  }
+};
+
+template < >
+struct pinModeStaticExpander< -1 >
+{
+  void operator() ( uint8_t const mode ) const {}
+};
+
+template < int num_channels, int num_brightness_levels >
+class CSoftPWM
+{
+public:
+  void begin(long hertz)
+  {
+    asm volatile ("/************ pinModeStaticExpander begin ************/");
+    uint8_t oldSREG = SREG;
+    cli();
+    pinModeStaticExpander< num_channels - 1 >()( OUTPUT );
+    SREG = oldSREG;
+    asm volatile ("/************ pinModeStaticExpander end ************/");
+
+    /* the setup of timer1 is stolen from ShiftPWM :-P
+     * http://www.elcojacobs.com/shiftpwm/ */
+    asm volatile ("/************ timer setup begin ************/");
+    bitSet(TCCR1B,WGM12);
+    bitClear(TCCR1B,WGM13);
+    bitClear(TCCR1A,WGM11);
+    bitClear(TCCR1A,WGM10);
+    bitSet(TCCR1B,CS10);
+    bitClear(TCCR1B,CS11);
+    bitClear(TCCR1B,CS12);
+    OCR1A = (F_CPU - hertz * brightnessLevels() / 2) / (hertz * brightnessLevels());
+    bitSet(TIMSK1,OCIE1A);
+    asm volatile ("/************ timer setup end ************/");
+
+    _count = 0;
+  }
+
+  void set( int channel_idx, uint8_t value )
+  {
+    _channels[ channel_idx ] = value;
+  }
+
+  size_t size() const { return num_channels; }
+  int brightnessLevels() const { return num_brightness_levels ? num_brightness_levels : 256; }
+
+  void allOff()
+  {
+    asm volatile ( "/********** CSoftPWM::allOff() begin **********/" );
+    uint8_t oldSREG = SREG;
+    cli();
+    for ( int i = 0; i < num_channels; ++i )
+      _channels[i] = 0;
+    bitWriteStaticExpander< num_channels - 1 >()( false );
+    SREG = oldSREG;
+    asm volatile ( "/********** CSoftPWM::allOff() end **********/" );
+  }
+  
+  /* this function cannot be private because the ISR uses it, and I have
+   * no idea about how to make friends with ISR. :-( */
+  void update() __attribute__((always_inline))
+  {
+    asm volatile ( "/********** CSoftPWM::update() begin **********/" );
+    uint8_t count = _count;
+    bitWriteStaticExpander< num_channels - 1 >()( count, _channels );
+    ++_count;
+    if ( _count == brightnessLevels() )
+      _count = 0;
+    asm volatile ( "/********** CSoftPWM::update() end **********/" );
+  }
+
+  /* this function is stolen from ShiftPWM :-P
+   * http://www.elcojacobs.com/shiftpwm/ */
+  void printInterruptLoad()
+  {
+    unsigned long time1, time2;
+
+    bitSet( TIMSK1, OCIE1A ); // enable interrupt
+    time1 = micros();
+    delayMicroseconds( 5000 );
+    time1 = micros() - time1;
+
+    bitClear( TIMSK1, OCIE1A ); // disable interrupt
+    time2 = micros();
+    delayMicroseconds( 5000 );
+    time2 = micros() - time2;
+
+    double load = static_cast< double >(time1 - time2) / time1;
+    double interrupt_frequency = static_cast< double >(F_CPU) / (OCR1A + 1);
+    double cycles_per_interrupt = load * F_CPU / interrupt_frequency;
+
+    Serial.println( F("SoftPWM::printInterruptLoad():") );
+    Serial.print( F("  Load of interrupt: ") );
+    Serial.println( load, 10 );
+    Serial.print( F("  Clock cycles per interrupt: ") );
+    Serial.println( cycles_per_interrupt );
+    Serial.print( F("  Interrupt frequency: ") );
+    Serial.print( interrupt_frequency );
+    Serial.println( F(" Hz") );
+    Serial.print( F("  PWM frequency: ") );
+    Serial.print( interrupt_frequency / brightnessLevels() );
+    Serial.println( F(" Hz") );
+    Serial.print( F("  Brightness levels: ") );
+    Serial.println( brightnessLevels() );
+    
+    bitSet( TIMSK1, OCIE1A ); // enable interrupt again
+  }
+
+private:
+  static void _timerCallback();
+
+  uint8_t _channels[ num_channels ];
+  uint8_t _count;
+};
+
+#endif
---
+++ examples/SoftPWM_example/SoftPWM_example.ino
@@ -0,0 +1,102 @@
+#include <SoftPWM.h>
+
+/* pins_arduino.h defines the pin-port/bit mapping as PROGMEM so
+ * you have to read them with pgm_read_xxx(). That's generally okay
+ * for ordinary use, but really bad when you're writing super fast
+ * codes because the compiler doesn't treat them as constants and
+ * cannot optimize them away with sbi / cbi instructions.
+ *
+ * Therefore we have to tell the compiler the PORT and BIT here.
+ * Hope someday we can find a way to workaround this.
+ *
+ * check the manuel of your MCU for port / bit mapping. */
+SOFTPWM_DEFINE_CHANNEL( 0, DDRC, PORTC, PORTC3 );
+SOFTPWM_DEFINE_CHANNEL( 1, DDRC, PORTC, PORTC2 );
+SOFTPWM_DEFINE_CHANNEL( 2, DDRD, PORTD, PORTD4 );
+SOFTPWM_DEFINE_CHANNEL( 3, DDRB, PORTB, PORTB6 );
+SOFTPWM_DEFINE_CHANNEL( 4, DDRB, PORTB, PORTB7 );
+SOFTPWM_DEFINE_CHANNEL( 5, DDRD, PORTD, PORTD5 );
+SOFTPWM_DEFINE_CHANNEL( 6, DDRD, PORTD, PORTD6 );
+SOFTPWM_DEFINE_CHANNEL( 7, DDRD, PORTD, PORTD7 );
+SOFTPWM_DEFINE_CHANNEL( 8, DDRB, PORTB, PORTB0 );
+SOFTPWM_DEFINE_CHANNEL( 9, DDRB, PORTB, PORTB1 );
+SOFTPWM_DEFINE_CHANNEL( 10, DDRB, PORTB, PORTB2 );
+SOFTPWM_DEFINE_CHANNEL( 11, DDRB, PORTB, PORTB3 );
+SOFTPWM_DEFINE_CHANNEL( 12, DDRB, PORTB, PORTB4 );
+SOFTPWM_DEFINE_CHANNEL( 13, DDRB, PORTB, PORTB5 );
+SOFTPWM_DEFINE_CHANNEL( 14, DDRC, PORTC, PORTC0 );
+SOFTPWM_DEFINE_CHANNEL( 15, DDRC, PORTC, PORTC1 );
+
+/* or you may want inverted outputs */
+/*
+SOFTPWM_DEFINE_CHANNEL_INVERT( 0, DDRC, PORTC, PORTC3 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 1, DDRC, PORTC, PORTC2 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 2, DDRD, PORTD, PORTD4 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 3, DDRB, PORTB, PORTB6 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 4, DDRB, PORTB, PORTB7 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 5, DDRD, PORTD, PORTD5 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 6, DDRD, PORTD, PORTD6 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 7, DDRD, PORTD, PORTD7 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 8, DDRB, PORTB, PORTB0 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 9, DDRB, PORTB, PORTB1 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 10, DDRB, PORTB, PORTB2 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 11, DDRB, PORTB, PORTB3 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 12, DDRB, PORTB, PORTB4 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 13, DDRB, PORTB, PORTB5 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 14, DDRC, PORTC, PORTC0 );
+SOFTPWM_DEFINE_CHANNEL_INVERT( 15, DDRC, PORTC, PORTC1 );
+*/
+
+/* Here you make an instance of desired channel counts you want
+ * with the default 256 brightness levels (0 ~ 255) */
+//SOFTPWM_DEFINE_OBJECT( 16 );
+
+/* Or you can make one with only 100 brightness levels (0 ~ 99).
+ * by using less brightness levels, you may be able to use higher
+ * pwm frequencies. */
+SOFTPWM_DEFINE_OBJECT_WITH_BRIGHTNESS_LEVELS( 16, 100 );
+
+/* if you want to use the SoftPWM object outside where it's defined,
+ * add the following line to the file. */
+//SOFTPWM_DEFINE_EXTERN_OBJECT( 16 );
+SOFTPWM_DEFINE_EXTERN_OBJECT_WITH_BRIGHTNESS_LEVELS( 16, 100 );
+
+void setup()
+{
+  Serial.begin( 19200 );
+
+  /* begin with 60hz pwm frequency */
+  SoftPWM.begin( 60 );
+  
+  /* print interrupt load for diagnostic purpose */
+  SoftPWM.printInterruptLoad();
+}
+
+static volatile uint8_t v = 0;
+void loop()
+{
+  long nextMillis = 0;
+
+  for (uint8_t i = 0;i < SoftPWM.size();++i)
+  {
+    Serial.print( micros() );
+    Serial.print( " loop(): " );
+    Serial.print( i );
+    Serial.println();
+
+    unsigned long const WAIT = 1000000 / SoftPWM.brightnessLevels() / 2;
+    unsigned long nextMicros = 0;
+    for (int v = 0;v <= SoftPWM.brightnessLevels() - 1; ++v)
+    {
+      while (micros() < nextMicros);
+      nextMicros = micros() + WAIT;
+      SoftPWM.set( i, v );
+    }
+    for (int v = SoftPWM.brightnessLevels() - 1; v >= 0; --v)
+    {
+      while (micros() < nextMicros);
+      nextMicros = micros() + WAIT;
+      SoftPWM.set( i, v );
+    }
+  }
+}
